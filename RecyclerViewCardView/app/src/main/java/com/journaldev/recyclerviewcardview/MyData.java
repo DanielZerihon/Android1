f0961dea15c6dc8a9be1074fdccf0f643919a494
danielzerihon <daniel92023@gmail.com>
2022-08-09 18:54:16 +0300

    adding the RecyclerViewCardView project

---
+++ RecyclerViewCardView/app/src/main/java/com/journaldev/recyclerviewcardview/MyData.java
@@ -0,0 +1,14 @@
+package com.journaldev.recyclerviewcardview;
+
+// כאן נמצא כל הדאטא שלי
+
+public class MyData {
+
+    static String[] nameArray = {"SonGoku", "Bulma", "Krillin", "Piccolo", "SonGohan", "Vegeta","Bardock", "Master Roshi", "Tien Shinhan"};
+    static String[] versionArray = {"his original name is Kakarrot.", "first appears as a teenager", "is a bald martial artist ", "being narrowly defeated", "He is kidnapped", "ast prince of the", "travel to Earth in search of the Dragon", "lives on Earth ", "King Cold prior", " the Dragon Team besides Goku","the Trunks from the series"};
+
+    static Integer[] drawableArray = {R.drawable.goku, R.drawable.bbulma, R.drawable.kririn, R.drawable.picolo, R.drawable.gohan, R.drawable.vegeta,
+            R.drawable.badrock, R.drawable.masterrusi,R.drawable.trunks};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
